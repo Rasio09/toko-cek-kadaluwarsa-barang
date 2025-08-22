e6b48e5c14f4a00972b71c3e14b343e568508762
Rasio09 <rasiovolume22@gmail.com>
2025-08-22 21:28:10 +0700

    update tambahkan navbar

--- barang.php
+++ barang.php
@@ -46,8 +46,53 @@ if ($search !== '') {
   <meta charset="UTF-8">
   <title>Daftar Barang</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
 <body class="p-4">
+
+<!-- Navbar Bootstrap -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">TokoApp</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarMain">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="listBarangDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            List Barang
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="listBarangDropdown">
+            <li><a class="dropdown-item" href="barang.php">Barang</a></li>
+            <li><a class="dropdown-item" href="tambah_barang.php">Tambah Barang</a></li>
+            <li><a class="dropdown-item" href="cek_barang.php">Cek Barang</a></li>
+            <li><a class="dropdown-item" href="record.php">Record Barang</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="brand.php">List Brand</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-person-circle fs-5"></i>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+            <li><a class="dropdown-item" href="profil.php">Profil</a></li>
+            <li><a class="dropdown-item" href="change_password.php">Change Password</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
 <div class="container">
   <h2>Daftar Barang Aktif</h2>
